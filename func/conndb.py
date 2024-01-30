d2f14eb72d3d7775ba04b596e2e40dfad361a1ad
Pedro Jimenez <pjb1091@gmail.com>
2024-01-29 22:58:01 -0600

    [CHANGE] Information file conndb

--- func/conndb.py
+++ func/conndb.py
@@ -1,9 +1,14 @@
 import json
-import logging
-from psycopg2 import connect as pg_connect , sql
+from psycopg2 import connect as pg_connect
 
 class dbcams:
+    """
+    The class is the base for connection with the postgres database
+    for client camssecurity.
 
+    :parameter: option <integer>
+    :return: conn <psycopg2.connect>
+    """
     option = 0
 
     def __int__(self, option):
@@ -24,12 +29,14 @@ class dbcams:
                     port = line["port"]
                     user = line["user"]
                     password = line["password"]
+                    # Choice de DB or Template connection / default DB Name
                     if option == 0:
                         dbname = line["dbname"]
                     elif option == 1:
                         dbname = line["template"]
                     else:
                         dbname = line["dbname"]
+                    # Try connection method by psycopg2
                     try:
                         self.conn = pg_connect(
                             "dbname='{}'"
@@ -45,7 +52,9 @@ class dbcams:
                             )
                         )
                         self.cursor = self.conn.cursor()
+                        # Run principal function son node
                         x = func(*args, {'conn': self.conn, 'cursor': self.cursor, 'data': data})
+                        # Close connection and cursor
                         self.cursor.close()
                         self.conn.close()
                         return x
@@ -56,11 +65,9 @@ class dbcams:
             except Exception as e:
                 return self.__error(display='Variable requerid : (data={}, option=0) <= Is most important', e=e)
             print(kwargs)
+
         return wraps
 
-    def __error(self,response=True, display='', e=Exception, data=None):
-        msj = '{} - {}'.format(display,e)
+    def __error(self, response=True, display='', e=Exception, data=None):
+        msj = '{} - {}'.format(display, e)
         return {'error': response, 'msj': msj, 'data': data}
-
-
-
