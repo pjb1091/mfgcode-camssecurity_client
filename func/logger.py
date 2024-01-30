3727c96d04e39e3d0f75718d9c136270746ada29
Pedro Jimenez <pjb1091@gmail.com>
2024-01-30 13:07:13 -0600

    [CHANGE] Add changes logger

--- func/logger.py
+++ func/logger.py
@@ -1,17 +1,16 @@
-import json
-from psycopg2 import connect as pg_connect , sql
+import logging
+class logger():
 
-class logger:
-
-    option = "static/logger.file"
-
-    def __int__(self, file=option):
-        self.option = file
+    def __int__(self):
+        logging.basicConfig(
+            d = {'clientip':'10.150.4.192'}
+            format='[%(levelname)s] %(process)d_%(processName)s %(asctime)s %(filename)s %(funcName)s %(module)s %(clientip)-15s %(user)-8s %(message)s %(msecs)d %(created)f',
+            datefmt='%m/%d/%Y_%I:%M:%S_%p', level=logging.DEBUG, extra=d)
 
     def __call__(self, func):
         def wraps(*args, **kwargs):
             try:
-                f = open(self.option, "a")
+                print("print log")
                 x = func(*args, **kwargs)
                 return  x
             except Exception as e:
